8decd5207dbf29c3619a8a04c04e7aa75a378b0d
yumasato-306 <yuma.sato0829@icloud.com>
2020-01-22 17:04:56 +0900

    message display

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -11,7 +11,6 @@ class ImageUploader < CarrierWave::Uploader::Base
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-    process resize_to_fit: [800, 800]
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
@@ -23,7 +22,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  # process scale: [200, 300]
+    process resize_to_fit: [800, 800]
   #
   # def scale(width, height)
   #   # do something
