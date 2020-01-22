118eb9ce1467ba5ed49f21ee92d5dfce54893e3d
yumasato-306 <yuma.sato0829@icloud.com>
2020-01-22 16:09:10 +0900

    send-function

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -11,6 +11,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    process resize_to_fit: [800, 800]
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
@@ -23,7 +24,6 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Process files as they are uploaded:
   # process scale: [200, 300]
-  process resize_to_fit: [800, 800]
   #
   # def scale(width, height)
   #   # do something
