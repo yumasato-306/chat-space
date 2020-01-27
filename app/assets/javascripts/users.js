492e591af9675063da40ae85394b6ce3db7800c0
yumasato-306 <yuma.sato0829@icloud.com>
2020-01-27 16:41:10 +0900

    fin

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -17,6 +17,18 @@ $(function() {
     `;
     $("#user-search-result").append(html);
   }
+  function addDeleteUser(name, id) {
+    let html = `
+    <div class="chat-group-user clearfix" id="${id}">
+      <p class="chat-group-user__name">${name}</p>
+      <div class="user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn" data-user-id="${id}" data-user-name="${name}">削除</div>
+    </div>`;
+    $(".js-add-user").append(html);
+  }
+  function addMember(userId) {
+    let html = `<input value="${userId}" name="group[user_ids][]" type="hidden" id="group_user_ids_${userId}" />`;
+    $(`#${userId}`).append(html);
+  }
   $("#user-search-field").on("keyup", function() {
     let input = $("#user-search-field").val();
     $.ajax({
@@ -27,6 +39,7 @@ $(function() {
     })
       .done(function(users) {
         $("#user-search-result").empty();
+
         if (users.length !== 0) {
           users.forEach(function(user) {
             addUser(user);
@@ -40,5 +53,20 @@ $(function() {
       .fail(function() {
         alert("通信エラーです。ユーザーが表示できません。");
       });
+  });
+  $(document).on("click", ".chat-group-user__btn--add", function() {
+    console.log
+    const userName = $(this).attr("data-user-name");
+    const userId = $(this).attr("data-user-id");
+    $(this)
+      .parent()
+      .remove();
+    addDeleteUser(userName, userId);
+    addMember(userId);
+  });
+  $(document).on("click", ".chat-group-user__btn--remove", function() {
+    $(this)
+      .parent()
+      .remove();
   });
 });
