435a78fd824cf1150a6dfe3841d7a1a21e1a6545
yumasato-306 <yuma.sato0829@icloud.com>
2020-01-28 11:32:18 +0900

    time_format

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -89,7 +89,6 @@ $('#new_message').on('submit', function(e){
       data: {id: last_message_id}
     })
     .done(function(messages) {
-      console.log(messages)
       if (messages.length !== 0) {
         var insertHTML = '';
         $.each(messages, function(i, message) {
