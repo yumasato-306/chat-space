17b8b54c488c7efdb7ae3b16c0dba3bceea38a92
yumasato-306 <yuma.sato0829@icloud.com>
2020-01-29 19:27:55 +0900

    Update messages_controller.rb

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,7 +8,7 @@ class MessagesController < ApplicationController
 
   def create
     @message = @group.messages.new(message_params)
-    if @message.save
+    if @message.save!
       respond_to do |format|
         format.html {redirect_to group_messages_path(@group,notice:"failed")}
         format.json
